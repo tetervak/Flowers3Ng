9d3bdffb406759f6482223c0f7ce25c1503f568a
Alex Tetervak <tetervak@sheridancollege.ca>
2019-11-29 14:36:44 -0500

    added FlowerDetailsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FooterComponent} from './footer.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FlowerDetailsComponent} from './flower-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    FlowerDetailsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
